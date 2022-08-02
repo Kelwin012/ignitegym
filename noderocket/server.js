d0e3beb4c7c709ed7057d5524d3340c40b9246e6
KingKelwin <kelwincarlos123@gmail.com>
2022-08-02 07:26:07 -0300

    Commitando as alterações par ao servidor

---
+++ noderocket/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if(request.url === '/produto'){
+        response.end(JSON.stringify({
+            message:"Rota de produto"
+        })
+        )
+    }
+
+    if (request.url === '/usuario'){
+        response.end(JSON.stringify({
+        message:"Rota de usuario"
+        })
+        )
+    }
+
+    response.end(JSON.stringify({
+        message:"Rota Padrão"
+    })
+    )
+
+}).listen(4001,() => console.log('Servidor está rodando na porta 4001'))
